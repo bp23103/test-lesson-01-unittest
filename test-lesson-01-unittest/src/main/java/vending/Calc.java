08acce4b83ccdbe8a34a538175bf28bc01b3c6ac
bp23103 <bp23103@shibaura-it.ac.jp>
2026-06-13 13:05:18 +0900

    Add files via upload

---
+++ test-lesson-01-unittest/src/main/java/vending/Calc.java
@@ -0,0 +1,12 @@
+package vending;
+
+/**
+ * 演習①-2（写経）で使う、いちばん簡単な部品。
+ * このファイルは完成しています。
+ */
+public class Calc {
+    /** 2つの数を足して返す */
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
